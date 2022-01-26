22cd66782a8f2d83aa0ee5c1370ab353a7eb0b95
FlooredAtHome <95613798+FlooredAtHome@users.noreply.github.com>
2022-01-26 15:25:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,9 @@
     }
     function successCallback(position) {
         toggleWatchBtn.innerHTML = "Stop Watching";
-        
+        var t1 = Date.now();
+        var lat1 = Math.round(position.coords.latitude * 10000.0) / 10000.0;
+        var lon1 = Math.round(position.coords.longitude * 10000.0) / 10000.0;
         // Check position has been changed or not before doing anything
         if(prevLat != position.coords.latitude || prevLong != position.coords.longitude) {
             
@@ -28,8 +30,38 @@
             var prevLat = Math.round(prevLatR * 10000.0) / 10000.0;
             var prevLong = Math.round(prevLongR * 10000.0) / 10000.0;
             
+            var lat2 = prevLat;
+            var lon2 = prevLong;
+            
+            //Speed Calculation
+            var t2 = Date.now();
+            if (typeof(Number.prototype.toRad) === "undefined") {
+                Number.prototype.toRad = function() {
+                return this * Math.PI / 180;
+                }
+            }
+            var R = 6371; // km
+            var dLat = (lat2-lat1).toRad();
+            var dLon = (lon2-lon1).toRad();
+            var lat1 = lat1.toRad();
+            var lat2 = lat2.toRad();
+
+            var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) *    Math.cos(lat2); 
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+            var distance = R * c;
+            // From cletus' answer ends
+            if(t1 == t2)
+            {
+                var spd = 0;
+            }
+            else
+            {
+                var spd = distance / (t2 - t1);
+            }
+            
             // Get current position
-            var positionInfo = "Your current position is (" + "Latitude: " + Math.round(position.coords.latitude * 10000.0) / 10000.0 + ", " + "Longitude: " + Math.round(position.coords.longitude * 10000.0) / 10000.0 + ")";
+            var positionInfo = "Your current position is (" + "Latitude: " + Math.round(position.coords.latitude * 10000.0) / 10000.0 + ", " + "Longitude: " + Math.round(position.coords.longitude * 10000.0) / 10000.0 + ", " + "Speed: " + spd + ")";
             document.getElementById("result").innerHTML = positionInfo;
             
         }
